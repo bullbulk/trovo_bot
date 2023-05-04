160a2ca7262609747d62c6144fc03ee3ef851ef0
Yuriy Grebenshchikov <grur.ura@mail.ru>
2023-05-04 19:50:41 +0500

    prevent diceamount to be negative

--- app/crud/crud_bot.py
+++ app/crud/crud_bot.py
@@ -13,7 +13,9 @@ class CRUDDiceAmount(CRUDBase[models.DiceAmount, CreateSchemaType, UpdateSchemaT
         db_obj: DiceAmount,
         amount: int,
     ) -> DiceAmount:
-        return super().update(db, db_obj=db_obj, obj_in={"amount": db_obj.amount - amount})
+        result_amount = db_obj.amount - amount
+        result_amount = max(result_amount, 0)
+        return super().update(db, db_obj=db_obj, obj_in={"amount": result_amount})
 
     def get_by_owner(self, db: Session, *, user_id: int) -> DiceAmount | None:
         return db.query(self.model).filter(DiceAmount.user_id == user_id).first()
