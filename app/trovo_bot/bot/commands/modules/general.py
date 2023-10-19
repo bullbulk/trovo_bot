6080c197ce5acc32daf5b51638da482187bb2161
Yuriy Grebenshchikov <grur.ura@mail.ru>
2023-10-19 14:00:02 +0500

    added roles commands

--- app/trovo_bot/bot/commands/modules/general.py
+++ app/trovo_bot/bot/commands/modules/general.py
@@ -1,5 +1,6 @@
-from app.trovo_bot.bot.trovo import TrovoApi
 from app.trovo_bot.bot.commands import Command
+from app.trovo_bot.bot.exceptions import IncorrectUsage
+from app.trovo_bot.bot.trovo import TrovoApi
 
 
 class HosepCommand(Command):
@@ -38,3 +39,58 @@ class InvokeCommand(Command):
         )
         data = await result.json()
         await api.send(data["display_msg"], message.channel_id)
+
+
+class AddroleCommand(Command):
+    """Добавить роль пользователям. Доступно только пользователям с ролью "модератор" и выше"""
+
+    name = "addrole"
+    moderator_only = True
+
+    usage = "!addrole <Роль> [*никнеймы]"
+    example = "!addrole Чемпион bullbulk G1deonTV"
+
+    async def handle(self, parts: list[str], message, db):
+        await super().handle(parts, message, db)
+
+        api = TrovoApi()
+        role = parts[1]
+        nicknames = parts[2:]
+
+        for nickname in nicknames:
+            nickname = nickname.removeprefix("@")
+            result = await api.command(
+                command=f"addrole {role} {nickname}", channel_id=message.channel_id
+            )
+            data = await result.json()
+            await api.send(data["display_msg"], message.channel_id)
+
+
+class RemoveroleCommand(Command):
+    """Удалить роль у пользователей. Доступно только пользователям с ролью "модератор" и выше"""
+
+    name = "removerole"
+    moderator_only = True
+
+    usage = "!removerole <Роль> [*никнеймы]"
+    example = "!removerole Чемпион bullbulk G1deonTV"
+
+    async def handle(self, parts: list[str], message, db):
+        await super().handle(parts, message, db)
+
+        args = parts[1:]
+
+        if len(args) < 2:
+            raise IncorrectUsage
+
+        api = TrovoApi()
+        role = args[0]
+        nicknames = args[1:]
+
+        for nickname in nicknames:
+            nickname = nickname.removeprefix("@")
+            result = await api.command(
+                command=f"removerole {role} {nickname}", channel_id=message.channel_id
+            )
+            data = await result.json()
+            await api.send(data["display_msg"], message.channel_id)
