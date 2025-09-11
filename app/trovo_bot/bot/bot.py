324efd7a85e176a9cb4325d8cb13363e4c7727e0
Yuriy Grebenshchikov <grur.ura@mail.ru>
2025-09-11 23:41:33 +0500

    added roles

--- app/trovo_bot/bot/bot.py
+++ app/trovo_bot/bot/bot.py
@@ -263,24 +263,6 @@ class Bot:
         if message.content.lower().startswith("@jarvisbot"):
             return await self.process_llm_request(message)
 
-        mage_roles = [
-            "НАЧИНАЮЩИЙ КОЛДУН",
-            "HAЧИНАЮЩИЙ КОЛДУН",
-            "HАЧИНАЮЩИЙ КОЛДУН",
-            "HАЧИНАЮЩИЙ КОЛДУH",
-        ]
-
-        mana_intersection = self.get_roles_intersection(message.roles, mage_roles)
-
-        if len(mana_intersection) > 1:
-            for i in range(1, len(mana_intersection)):
-                await self.revoke_role(
-                    message.nick_name,
-                    mana_intersection[i],
-                    message.channel_id,
-                    send_message=False,
-                )
-
 
     async def process_command(self, message: Message):
         content_parts = message.content.removeprefix("!").split()
